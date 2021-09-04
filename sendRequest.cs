dfb73480a2194e2aaa5a6598d8eb7be00da77ca6
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-03 22:28:05 -0700

    Tested and compiled without issue

--- sendRequest.cs
+++ sendRequest.cs
@@ -1,19 +1,23 @@
 using System;
+using System.Net.Http;
+using System.Collections.Generic;
 
 public class CPHInline
 {
+    private static readonly HttpClient client = new HttpClient();
     public bool Execute()
     {
-		CPH.SendWhisper("melonlore", "TestWhisper");
+        var postValues = new Dictionary<string, string>{};
+        CPH.SendWhisper("melonlore", "TestWhisper");
         foreach (var arg in args)
         {
-			CPH.SendWhisper("melonlore", $"LogVars :: {arg.Key} = {arg.Value}");
+            CPH.SendWhisper("melonlore", $"LogVars :: {arg.Key} = {arg.Value}");
             CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
             CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
-			if (arg.Key == "rawInput")
-			{
-				CPH.SendMessage($"{arg.Value}");
-			}
+            if (arg.Key == "rawInput")
+            {
+                CPH.SendMessage($"{arg.Value}");
+            }
         }
 
         return true;
