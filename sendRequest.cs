cf6e7d5e6c992b1d2efa64f54dbf568f808162e7
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-03 23:17:08 -0700

    formatting and compile test

--- sendRequest.cs
+++ sendRequest.cs
@@ -4,26 +4,26 @@ using System.Collections.Generic;
 
 public class CPHInline
 {
-    private static readonly HttpClient client = new HttpClient();
-	 private static readonly String targetURL = "localhost:8080";
-    public bool Execute()
-    {
-        var postValues = new Dictionary<string, string>{};
-        CPH.SendWhisper("melonlore", "TestWhisper");
-        foreach (var arg in args)
-        {
-            CPH.SendWhisper("melonlore", $"LogVars :: {arg.Key} = {arg.Value}");
-            CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
-            CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
-            if (arg.Key == "rawInput")
-            {
-					 postValues.Add($"{arg.Key}", $"{arg.Value}");
-                CPH.SendMessage($"{arg.Value}");
-            }
-        }
-		//   send the request off
-		 
+   private static readonly HttpClient client = new HttpClient();
+   private static readonly String targetURL = "localhost:8080";
+   public bool Execute()
+   {
+      var postValues = new Dictionary<string, string> { };
+      CPH.SendWhisper("melonlore", "TestWhisper");
+      foreach (var arg in args)
+      {
+         CPH.SendWhisper("melonlore", $"LogVars :: {arg.Key} = {arg.Value}");
+         CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
+         CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
+         if (arg.Key == "rawInput")
+         {
+            postValues.Add($"{arg.Key}", $"{arg.Value}");
+            CPH.SendMessage($"{arg.Value}");
+         }
+      }
 
-        return true;
-    }
+      //   send the request off
+      var content = new FormUrlEncodedContent(postValues);
+      return true;
+   }
 }
