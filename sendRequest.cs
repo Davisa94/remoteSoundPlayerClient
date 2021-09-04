d61edd20e5f0f22aec9934ebdbb62b29697dfd11
Davisa94 <Austin.i.davis94@gmail.com>
2021-09-03 22:43:16 -0700

    fixed a few formatting issues

--- sendRequest.cs
+++ sendRequest.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class CPHInline
 {
     private static readonly HttpClient client = new HttpClient();
+	 private static readonly String targetURL = "localhost:8080";
     public bool Execute()
     {
         var postValues = new Dictionary<string, string>{};
@@ -16,9 +17,12 @@ public class CPHInline
             CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
             if (arg.Key == "rawInput")
             {
+					 postValues.Add($"{arg.Key}", $"{arg.Value}");
                 CPH.SendMessage($"{arg.Value}");
             }
         }
+		//   send the request off
+		 
 
         return true;
     }
